72bfb3851c6e1e95c8e050bb62b21595aac1be7d
jiminKang <wlals1234560@gmail.com>
2025-04-15 16:48:03 +0900

    stoi 메서드에 인자로 공백이 포함되어 런타임 에러가 발생하였음. stoi의 인자로 substr 메서드를 이용한 인자를 전달하여 런타임에러 해결

--- Data_Structure/Hash/19583.cpp
+++ Data_Structure/Hash/19583.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-#include <cmath>
 
 using namespace std;
 
-string S, E, Q;
+int timeToMinutes(const string &t)
+{
+    return stoi(t.substr(0, 2)) * 60 + stoi(t.substr(3, 2));
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    unordered_map<string, bool> HashT;
-    int count = 0;
 
+    string S, E, Q;
     cin >> S >> E >> Q;
-    int Shour = stoi(string(1, S[0]) + string(1, S[1]));
-    int SMinute = stoi(string(1, S[3]) + string(1, S[4]));
-    int Stime = 60 * Shour + SMinute;
-    int Ehour = stoi(string(1, E[0]) + string(1, E[1]));
-    int EMinute = stoi(string(1, E[3]) + string(1, E[4]));
-    int Etime = 60 * Ehour + EMinute;
-    int Qhour = stoi(string(1, Q[0]) + string(1, Q[1]));
-    int QMinute = stoi(string(1, Q[3]) + string(1, Q[4]));
-    int Qtime = 60 * Qhour + QMinute;
-    string chatTime;
-    string nickname;
-    while (cin >> chatTime >> nickname)
+
+    int Stime = timeToMinutes(S);
+    int Etime = timeToMinutes(E);
+    int Qtime = timeToMinutes(Q);
+
+    unordered_map<string, bool> entered;
+    int count = 0;
+    string chattime, nickname;
+
+    while (cin >> chattime >> nickname)
     {
+        if (chattime.length() != 5 || chattime[2] != ':')
+            continue;
 
-        int Chour = stoi(string(1, chatTime[0]) + string(1, chatTime[1]));
-        int CMinute = stoi(string(1, chatTime[3]) + string(1, chatTime[4]));
-        int Ctime = 60 * Chour + CMinute;
-
-        // chattim이 Q보다 크면 반복문 종료
-        if (Ctime > Qtime)
-        {
-            break;
-        }
+        int Ctime = timeToMinutes(chattime);
 
         if (Ctime <= Stime)
         {
-            HashT[nickname] = true;
+            entered[nickname] = true;
         }
-
-        // chatime이 E<=Q인 경우 출석처리
-        // 이전에 채팅한 이력이 S<=E 사이에 유효한 채팅한 이력이 있으면 출석처리
-        if (Etime <= Ctime && Ctime <= Qtime)
+        else if (Etime <= Ctime && Ctime <= Qtime)
         {
-            if (HashT[nickname])
+            if (entered[nickname])
             {
                 count++;
-                HashT[nickname] = false;
+                entered[nickname] = false;
             }
-
-            // cout << "nickname =" << nickname << '\n';
         }
     }
-    cout << count;
+
+    cout << count << '\n';
+    return 0;
 }
