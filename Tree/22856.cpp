07a5c4649055ee6719c679b2a4b2591bda998da7
jiminKang <wlals1234560@gmail.com>
2025-04-17 00:12:06 +0900

    테스트 케이스는 통과헀으나, 아직 틀렸다고함

--- Tree/22856.cpp
+++ Tree/22856.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
-using namespace std;
-#define max 100000
+#include <vector>
+#define max 100001
 
-int tree[max][2];
+using namespace std;
+
 int N;
+vector<int> Tree[max];
+vector<int> visitRootNode;
+vector<bool> visited;
+vector<int> parentNode;
 int ans = 0;
-void inOrder(int node);
+
+// 마지막 방문 노드를 알아내기 위함
+void inorder(int node)
+{
+    if (node == -1)
+    {
+        return;
+    }
+    visitRootNode.push_back(node);
+    for (int child : Tree[node])
+    {
+        inorder(child);
+    }
+}
+
 int main()
 {
     cin >> N;
+    visited.resize(N + 1, false);
+    parentNode.resize(N + 1, 0);
     for (int i = 0; i < N; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        tree[a][0] = b;
-        tree[a][1] = c;
+        Tree[a].push_back(b);
+        Tree[a].push_back(c);
+        parentNode[b] = a;
+        parentNode[c] = a;
     }
-    inOrder(1);
+
+    inorder(1);
+    int lastNode = visitRootNode.back();
+
+    int cur = 1;
+    visited[cur] = true;
+    while (cur != lastNode)
+    {
+        if (!visited[Tree[cur][0]] && Tree[cur][0] != -1)
+        {
+            cur = Tree[cur][0];
+        }
+        else if (!visited[Tree[cur][1]] && Tree[cur][1] != -1)
+        {
+
+            cur = Tree[cur][1];
+        }
+        else
+        {
+            cur = parentNode[cur];
+        }
+        ans++;
+        visited[cur] = true;
+    }
 
     cout << ans;
-}
-void inOrder(int node)
-{
-    if (node == -1)
-        return;
-    ans++;
-
-    // 재귀를 끝내고 부모 노드로 다시 돌아갈때 , 이동수를 증가 ?
-    // => 우측방문 하는 경우는 최하단 에 접근했을떄 끝내야함
-    inOrder(tree[node][0]);
-    // inOrder(tree[node][1]);
-
-    // 되돌아가는 수 증가
-    ans++;
 }
