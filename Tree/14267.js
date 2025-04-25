b3033a09ce088f123becdcd0b133509e3c6ec524
jiminKang <wlals1234560@gmail.com>
2025-04-25 15:50:53 +0900

    14267 리팩터링

--- Tree/14267.js
+++ Tree/14267.js
@@ -1,54 +1,42 @@
 const readline = require('readline');
 
-const rl = readline.Interface({
-    input:process.stdin,
-    output:process.stdout,
+const rl = readline.createInterface({  // 오타 수정: Interface → createInterface
+    input: process.stdin,
+    output: process.stdout,
 });
 
 const input = [];
-rl.on("line",(line)=>{
+rl.on("line", (line) => {
     input.push(line.trim());
-}).on("close",()=>{
+}).on("close", () => {
     const [N, M] = input[0].split(' ').map(Number);
     const treeinput = input[1].split(' ').map(Number);
     const tree = Array.from({ length: N }, () => []);
-    const complement = input.slice(2,2+M).map(line=> line.split(' ').map(Number));
     const complementSum = Array(N).fill(0);
-    const AnsComplementSum = Array(N).fill(0);
 
-    function setTree(tree,treeinput){
-        for(let i=0;i<treeinput.length;i++){
-            let childnode = treeinput[i];
-            if(childnode==-1) continue;
-            tree[childnode-1].push(i);
+    // 트리 구성
+    for (let i = 0; i < N; i++) {
+        const parent = treeinput[i];
+        if (parent !== -1) {
+            tree[parent - 1].push(i);
         }
     }
 
-    setTree(tree,treeinput);
-
-    function setCompelementSum () {
-        complement.forEach(e=>{
-            complementSum[e[0]-1]+=e[1];
-        })
+    // 칭찬 누적합 입력
+    for (let i = 2; i < 2 + M; i++) {
+        const [employee, praise] = input[i].split(' ').map(Number);
+        complementSum[employee - 1] += praise;
     }
-    setCompelementSum();
 
-    function calculateComplement(){
-        const DFS = (node,acc)=>{
-            let sum = complementSum[node];
-            AnsComplementSum[node]+= sum + acc;
-            for(let i=0;i<tree[node].length;i++){
-                DFS(tree[node][i],sum+acc);
-            }
+    // 누적 DFS
+    const DFS = (node, acc) => {
+        const total = acc + complementSum[node];
+        complementSum[node] = total;
+        for (const child of tree[node]) {
+            DFS(child, total);
         }
-        DFS(0,0);
-        
-    }
+    };
+    DFS(0, 0);
 
-    calculateComplement();
-
-
-    console.log(AnsComplementSum.join(' '));
-
-    
-})
+    console.log(complementSum.join(' '));
+});
